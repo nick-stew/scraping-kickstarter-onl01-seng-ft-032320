5813adbaf1113807d9dbcba994bc7823709099e4
github username <github email address>
2020-06-26 11:01:05 -0400

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,22 @@
-# require libraries/modules here
+require 'nokogiri'
+require 'pry'
 
 def create_project_hash
-  # write your code here
+  html = File.read("fixtures/kickstarter.html")
+  kickstarter = Nokogiri::HTML(html)
+
+  projects = {}
+
+  #iterate through projects
+  kickstarter.css("li.project.grid_4").each do |p|
+    title = p.css("h2.bbcard_name strong a").text
+    #turn title into symbol and create nested hash of values
+    projects[title.to_sym] = {
+   :image_link => p.css("div.project-thumbnail a img").attribute("src").value,
+   :description => p.css("p.bbcard_blurb").text,
+   :location => p.css("li a span.location-name").children.first.text,
+   :percent_funded => p.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+    }
+  end
+  projects
 end
